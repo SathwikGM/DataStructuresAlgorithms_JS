aa62416fdc111ae66a9b165a82aa4b35da30855c
sathwikgm <sathwikgm14@gmail.com>
2023-04-30 21:31:19 +0530

    added stack

---
+++ src/Data_Structures/Stacks/stacks.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  // adding at beginning of stack
+  push(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+    else {
+      let currentNode = this.first;
+      this.first = newNode;
+      this.first.next = currentNode;
+    }
+    return this.length++;
+  }
+  //  removing at end of stack
+  pop() {
+    if (this.length === 0) return null;
+    let removenode = this.first
+    if (this.length === 1) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return removenode.value;
+  }
+}
+
+let stack = new Stack();
